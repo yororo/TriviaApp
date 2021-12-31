bde0e4d5fdd16beb5fa96e372bd56f51629c4c61
Sean Yoro <sean.yoro@mindzie.com>
2021-12-31 14:48:27 +0800

    add basic question template

---
+++ TriviaApp/TriviaApp.Web/Pages/Index.razor.cs
@@ -0,0 +1,18 @@
+﻿using TriviaApp.Web.ViewModels;
+
+namespace TriviaApp.Web.Pages
+{
+    public partial class Index
+    {
+        public TriviaQuestionSetupViewModel TriviaQuestionSetupViewModel { get; set; }
+        public List<TriviaQuestionViewModel> Questions { get; set; }
+
+        public string? Question1Answer { get; set; }
+
+        public Index()
+        {
+            TriviaQuestionSetupViewModel = new TriviaQuestionSetupViewModel();
+            Questions = new List<TriviaQuestionViewModel>();
+        }
+    }
+}
---
+++ TriviaApp/TriviaApp.Web/ViewModels/TriviaQuestionSetupViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TriviaApp.Web.ViewModels
+{
+    public class TriviaQuestionSetupViewModel
+    {
+        public string Category { get; set; }
+        public string Difficulty { get; set; }
+        public string NumberOfQuestions { get; set; }
+    }
+}
